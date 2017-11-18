9730a9b496c09c2d64a0011f45eaea1589324169
Chris Casola <chriscasola@gmail.com>
2017-11-18 11:24:57 -0500

    Add a simple CORS middleware that allows everything

---
+++ middleware/cors/cors.go
@@ -0,0 +1,28 @@
+package cors
+
+import (
+	"net/http"
+)
+
+// Middleware is a middleware that adds handling for CORS requests
+type Middleware struct {
+	handler http.Handler
+}
+
+// New construct a new Cors middleware
+func New(handler http.Handler) *Middleware {
+	return &Middleware{handler: handler}
+}
+
+func (c *Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
+		w.Header().Set("Access-Control-Max-Age", "86400")
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		c.handler.ServeHTTP(w, r)
+	}
+}
